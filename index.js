49497631202136b4cbf95129a59ced650e3f44a2
Cloudymolecule <baires82@gmail.com>
2021-02-18 09:32:52 -0500

    commit tree 7a1274817cb6cdb013a20632a31db65bfd0a8460

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string){
+  const timeProv = string.split(':')
+  const hours = parseInt(timeProv[0])
+
+  if (hours < 12){
+    return 'Good Morning'
+  }
+
+  if (hours >= 12 && hours < 17){
+    return 'Good Afternoon'
+  }
+
+  if (hours >= 17){
+    return 'Good Evening'
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  greeting = document.getElementById('greeting')
+  greeting.innerText = string
+}
